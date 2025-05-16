42c447378c1005137cc8f481cf9f56c49c27aa5f
mahmudulashev <urgutmahmud@gmail.com>
2025-05-16 18:00:18 +0500

    bot

--- bot.js
+++ bot.js
@@ -1,12 +1,10 @@
 document.getElementById('contact-form').addEventListener('submit', function(e) {
   e.preventDefault();
 
-  // Har safar qiymatlarni yangidan oling
   const name = document.getElementById('name').value.trim();
   const phone = document.getElementById('phone').value.trim();
   const message = document.getElementById('Contact-Message').value.trim();
 
-  // Faqat hammasi to'ldirilganda yuboriladi va reset qilinadi
   fetch(`https://api.telegram.org/7526214683:AAF-px-S4wDzKOsxFS81sc1mGjropn4vtac/sendMessage`, {
     method: 'POST',
     headers: {'Content-Type': 'application/json'},
